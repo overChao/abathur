6872ee368f675500a4d84a7fda087f72867de961
zhangkc <zhangkechao@medatc.com>
2020-12-10 12:07:30 +0800

    leetcode 20201210.12

--- src/main/java/com/crodi/leetcode/leetcode/arrsys/Solution.java
+++ src/main/java/com/crodi/leetcode/leetcode/arrsys/Solution.java
@@ -1,7 +1,5 @@
 package com.crodi.leetcode.leetcode.arrsys;
 
-import java.util.Arrays;
-
 /**
  * @Package: com.crodi.leetcode.leetcode.arrsys
  * @Author: crodi
@@ -16,18 +14,39 @@ public class Solution {
 
         int[] result = new int[nums.length];
 
-        for (int i = 0; i < nums.length/2; i++) {
+        for (int i = 0; i < nums.length / 2; i++) {
             result[2 * i] = nums[i];
-            result[2 * i + 1] = nums[nums.length + i - nums.length/2];
+            result[2 * i + 1] = nums[nums.length + i - nums.length / 2];
         }
         return result;
+    }
+
+    /**
+     * leetCode 20201210
+     *
+     * @param s
+     * @param indices
+     * @return
+     */
+    public static String restoreString(String s, int[] indices) {
+
+        byte[] bytes = new byte[indices.length];
+        byte[] sBytes = s.getBytes();
+        for (int i = 0; i < indices.length; i++) {
+            bytes[indices[i]] = sBytes[i];
+        }
+        return new String(bytes);
     }
 
 
     public static void main(String[] args) {
 
-        int[] nums = new int[]{1,1,2,2};
-        System.out.println(Arrays.toString(shuffle(nums, 2)));
+        int[] nums = new int[]{1, 1, 2, 2};
+
+        int[] indices = new int[]{4, 5, 6, 7, 0, 2, 1, 3};
+
+        System.out.println(restoreString("codeleet", indices));
+//        System.out.println(Arrays.toString(shuffle(nums, 2)));
 
     }
 
